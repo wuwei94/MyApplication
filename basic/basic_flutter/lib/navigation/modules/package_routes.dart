6193b2f11a2107bf78403048d45293a2c38878e2
WilliamWu <wuwei94@yeah.net>
2026-03-17 14:07:15 +0800

    flutter ImagePicker

--- basic/basic_flutter/lib/navigation/modules/package_routes.dart
+++ basic/basic_flutter/lib/navigation/modules/package_routes.dart
@@ -1,10 +1,12 @@
 import 'package:basic_flutter/features/packages/dio_example.dart';
 import 'package:basic_flutter/features/packages/http_example.dart';
+import 'package:basic_flutter/features/packages/image_picker_example.dart';
 import 'package:basic_flutter/features/packages/notification_example.dart';
 import 'package:basic_flutter/features/packages/permission_example.dart';
 import 'package:basic_flutter/features/packages/screen_util_example.dart';
 import 'package:basic_flutter/features/packages/shared_preferences_example.dart';
 import 'package:basic_flutter/features/packages/toast_example.dart';
+import 'package:basic_flutter/features/packages/wechat_picker_example.dart';
 import 'package:basic_flutter/navigation/models/route_item_model.dart';
 import 'package:flutter/widgets.dart';
 
@@ -34,6 +36,18 @@ final List<RouteItem> packageRoutes = [
     title: 'Permission',
     subtitle: 'Permission',
     routeBuilder: (BuildContext context, _) => const PermissionExample(),
+  ),
+  RouteItem(
+    path: '/package/image-picker',
+    title: 'ImagePicker',
+    subtitle: 'ImagePicker',
+    routeBuilder: (BuildContext context, _) => const ImagePickerExample(),
+  ),
+  RouteItem(
+    path: '/package/wechat-picker',
+    title: 'WechatPicker',
+    subtitle: 'WechatPicker',
+    routeBuilder: (BuildContext context, _) => const WechatPickerExample(),
   ),
   RouteItem(
     path: '/package/shared-preferences',
