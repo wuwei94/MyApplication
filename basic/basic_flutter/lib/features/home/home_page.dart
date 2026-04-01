74fdb532c4356e9a498b1f2a1f6b3bcf4f8c1044
WilliamWu <wuwei94@yeah.net>
2026-04-01 18:47:57 +0800

    flutter fixes

--- basic/basic_flutter/lib/features/home/home_page.dart
+++ basic/basic_flutter/lib/features/home/home_page.dart
@@ -1,6 +1,7 @@
-import 'package:basic_flutter/features/features_list_page.dart';
 import 'package:basic_flutter/navigation/registry/route_registry.dart';
+import 'package:basic_flutter/navigation/utils/route_converter.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 /// 首页 - 分组入口
 class HomePage extends StatelessWidget {
@@ -18,17 +19,7 @@ class HomePage extends StatelessWidget {
             title: Text(group.title),
             subtitle: Text(group.subtitle),
             trailing: const Icon(Icons.chevron_right),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute<dynamic>(
-                  builder: (context) => FeaturesListPage(
-                    title: group.title,
-                    routes: group.routeItems,
-                  ),
-                ),
-              );
-            },
+            onTap: () => context.push(RouteConverter.groupPath, extra: group),
           );
         },
       ),
--- basic/basic_flutter/lib/navigation/app_router.dart
+++ basic/basic_flutter/lib/navigation/app_router.dart
@@ -1,4 +1,3 @@
-import 'package:basic_flutter/features/home/home_page.dart';
 import 'package:basic_flutter/navigation/modules/index.dart';
 import 'package:basic_flutter/navigation/utils/route_converter.dart';
 import 'package:go_router/go_router.dart';
@@ -8,13 +7,17 @@ final GoRouter appRouter = GoRouter(
   initialLocation: '/',
   routes: [
     // 首页路由
-    GoRoute(path: '/', builder: (context, state) => const HomePage()),
+    RouteConverter.getHomeRoute(),
+    // 分组路由
+    RouteConverter.getGroupRoute(),
     // 示例路由
     ...RouteConverter.toGoRoutes(exampleRoutes),
     // 网络请求路由
     ...RouteConverter.toGoRoutes(networkRoutes),
     // 本地存储路由
     ...RouteConverter.toGoRoutes(storageRoutes),
+    // 动画组件路由
+    ...RouteConverter.toGoRoutes(animRoutes),
     // 三方组件包路由
     ...RouteConverter.toGoRoutes(packageRoutes),
     // 状态管理路由
