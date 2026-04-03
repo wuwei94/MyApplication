4b0ecf62ab3cdb3543c348c9e6e18f7775af87a0
WilliamWu <wuwei94@yeah.net>
2026-04-03 15:51:14 +0800

    flutter auto_route

---
+++ basic/basic_flutter/lib/navigation/app_auto_router.dart
@@ -0,0 +1,31 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:basic_flutter/navigation/modules/index.dart';
+import 'package:basic_flutter/navigation/utils/route_converter.dart';
+import 'package:flutter/widgets.dart';
+
+// 路由模块化组织
+final RootStackRouter _appAutoRootRouter = RootStackRouter.build(
+  routes: [
+    // 首页路由
+    RouteConverter.getAutoHomeRoute(),
+    // 分组路由
+    RouteConverter.getAutoGroupRoute(),
+    // 示例路由
+    ...RouteConverter.toAutoRoutes(exampleRoutes),
+    // 网络请求路由
+    ...RouteConverter.toAutoRoutes(networkRoutes),
+    // 本地存储路由
+    ...RouteConverter.toAutoRoutes(storageRoutes),
+    // 动画组件路由
+    ...RouteConverter.toAutoRoutes(animRoutes),
+    // 三方组件包路由
+    ...RouteConverter.toAutoRoutes(packageRoutes),
+    // 视频组件路由
+    ...RouteConverter.toAutoRoutes(videoRoutes),
+    // 状态管理路由
+    ...RouteConverter.toAutoRoutes(stateManagerRoutes),
+  ],
+);
+
+final RouterConfig<Object> appAutoRouter =
+    _appAutoRootRouter.config() as RouterConfig<Object>;
