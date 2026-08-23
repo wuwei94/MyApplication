a9c32f46c4426cc3fcc08ec4576f9de32a39c9fa
WilliamWu <wuwei94@yeah.net>
2026-08-23 19:22:52 +0800

    refactor(mavericks): decouple from module_arch by inlining ArticleAdapter
    - Move ArticleAdapter into module_mavericks (article.adapter package)
    
    - Copy arch_item_recycler layout as mavericks_item_recycler
    
    - Remove implementation(project(":modules:module_arch")) from build.gradle.kts

--- modules/module_mavericks/build.gradle.kts
+++ modules/module_mavericks/build.gradle.kts
@@ -17,8 +17,5 @@ dependencies {
 
     implementation(project(":basic:basic_repo"))
 
-    // Mavericks 文章示例复用 module_arch 中的通用文章适配器
-    implementation(project(":modules:module_arch"))
-
     implementation(libs.mavericks)
 }
--- modules/module_mavericks/src/main/java/com/example/william/my/module/mavericks/article/ArticleMavericksFragment.kt
+++ modules/module_mavericks/src/main/java/com/example/william/my/module/mavericks/article/ArticleMavericksFragment.kt
@@ -6,7 +6,7 @@ import com.chad.library.adapter4.BaseQuickAdapter
 import com.chad.library.adapter4.viewholder.QuickViewHolder
 import com.example.william.my.basic.basic_repo.bean.ArticleDetailData
 import com.example.william.my.core.base.recycler.BaseRecyclerFragment
-import com.example.william.my.module.arch.adapter.ArticleAdapter
+import com.example.william.my.module.mavericks.article.adapter.ArticleAdapter
 import com.example.william.my.module.mavericks.article.data.ArticleMavericksState
 import com.example.william.my.module.mavericks.article.viewmodel.ArticleMavericksViewModel
 
