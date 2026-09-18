c682783751b22c70edf9f9af541aae7f5d342cef
WilliamWu <wuwei94@yeah.net>
2026-09-18 19:19:13 +0800

    refactor(module_event): 能力包下按 activity/data 角色分层
    示例 Activity 归入能力包 activity/，支撑实现归入 data/；入口与 @Route 路由不变。

--- modules/module_event/src/main/java/com/example/william/my/module/event/eventbus/activity/EventBusActivity.kt
+++ modules/module_event/src/main/java/com/example/william/my/module/event/eventbus/activity/EventBusActivity.kt
@@ -1,4 +1,4 @@
-package com.example.william.my.module.event.activity
+package com.example.william.my.module.event.eventbus.activity
 
 import android.os.Bundle
 import com.alibaba.android.arouter.facade.annotation.Route
--- modules/module_event/src/main/java/com/example/william/my/module/event/floweventbus/activity/FlowEventBusActivity.kt
+++ modules/module_event/src/main/java/com/example/william/my/module/event/floweventbus/activity/FlowEventBusActivity.kt
@@ -1,4 +1,4 @@
-package com.example.william.my.module.event.activity
+package com.example.william.my.module.event.floweventbus.activity
 
 import android.os.Bundle
 import com.alibaba.android.arouter.facade.annotation.Route
